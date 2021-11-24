56586220d8a6de9dfe6e933dc336bff2c4f02c76
duck <godqhr2256@gmail.com>
2021-11-24 17:45:48 +0900

    label, classname 추가

--- 끝말잇기/Wordgame_hook.jsx
+++ 끝말잇기/Wordgame_hook.jsx
@@ -30,7 +30,8 @@ const WordGame = () =>{
             <>
                 <div>{word}</div>
                 <form onSubmit={onSubmitForm}>
-                    <input ref ={inputRef} value={value} onChange={onChangeInput} />
+                    <label id ="label" htmlFor="wordInput">글자를 입력하세요</label>
+                    <input id="wordInput" className="wordInput"  ref ={inputRef} value={value} onChange={onChangeInput} />
                     <button>입력!</button>
                     <div>{result}</div>
                 </form>
