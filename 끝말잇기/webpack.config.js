46a834d5c03c92feaa71963a3ad15275bf6b32bd
duck <godqhr2256@gmail.com>
2021-11-24 16:18:27 +0900

    웹팩 세팅 추가

--- 끝말잇기/webpack.config.js
+++ 끝말잇기/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { webpack } = require('webpack');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 
 module.exports ={
     name:'word-Game-setting',
@@ -9,19 +11,29 @@ module.exports ={
     },
     
     entry:{ //  입력
-        app:['./client'],
+        app:'./client',
     },
     module:{
         rules:[{
             test: /\.js$|jsx/, // 정규표현식
             loader: 'babel-loader',
             options:{ 
-                presets:['@babel/preset-env','@babel/preset-react'],
+                presets:[
+                ['@babel/preset-env',{
+                    targets:{
+                        browsers:['> 1% in KR'],
+                    },
+                    debug:true,
+                }],
+                '@babel/preset-react',
+                ],
                 plugins:['@babel/plugin-proposal-class-properties']
             }
         }]
-    }, 
-
+    },
+    plugins:[
+        new LoaderOptionsPlugin({debug:true}),
+    ], 
     output:{ // 출력
         path: path.join(__dirname,'dist'), // 경로를 합쳐줌.
         filename: 'app.js'
