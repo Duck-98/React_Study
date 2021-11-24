7c0932a0f4e1229c10b60336fc4799bc1c467647
duck <godqhr2256@gmail.com>
2021-11-24 16:46:32 +0900

    웹 팩 데브서버 설정 및 핫 리로딩

--- 끝말잇기/webpack.config.js
+++ 끝말잇기/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { webpack } = require('webpack');
 const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+const Refreshwebpack = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports ={
     name:'word-Game-setting',
@@ -27,15 +28,21 @@ module.exports ={
                 }],
                 '@babel/preset-react',
                 ],
-                plugins:['@babel/plugin-proposal-class-properties']
+                plugins:['@babel/plugin-proposal-class-properties','react-refresh/babel']
             }
         }]
     },
     plugins:[
         new LoaderOptionsPlugin({debug:true}),
+        new Refreshwebpack({overlay:false}),
     ], 
     output:{ // 출력
         path: path.join(__dirname,'dist'), // 경로를 합쳐줌.
-        filename: 'app.js'
+        filename: 'app.js',     
     },
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+      }
 };
