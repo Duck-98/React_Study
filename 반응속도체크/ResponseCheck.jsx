24fc4344c8543b7cfa2872c6c7b5736ae50d92c5
duck <godqhr2256@gmail.com>
2021-11-26 17:38:14 +0900

    반응속도

--- 반응속도체크/ResponseCheck.jsx
+++ 반응속도체크/ResponseCheck.jsx
@@ -2,48 +2,62 @@ const React = require('react');
 const {Component} = React;
 
 class ResponseCheck extends Component {
-    state={
-        word:'끝말잇기',
-        value:'',
-        result:'',
-   };
-    onSubmitForm = (e) =>{
-        e.preventDefault();
-        if(this.state.word[this.state.word.length - 1 ] === this.state.value[0]){
+    state ={
+        state:'waiting',
+        message:'클릭해서 시작',
+    };
+
+    onClickScreen = () => {
+        const { state } = this.state;
+        if (state === 'waiting') {
+          timeout.current = setTimeout(() => {
             this.setState({
-                result: '딩동댕',
-                word : this.state.value,
-                value: '',
+              state: 'now',
+              message: '지금 클릭',
             });
-            this.input.focus();
-        }else{
-            this.setState({
-                result:'땡',
-                value : '',
-            })
-            this.input.focus();
+            this.startTime = new Date();
+          }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
+          this.setState({
+            state: 'ready',
+            message: '초록색이 되면 클릭하세요.',
+          });
+        } else if (state === 'ready') { // 성급하게 클릭
+          clearTimeout(this.timeout);
+          this.setState({
+            state: 'waiting',
+            message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요.',
+          });
+        } else if (state === 'now') { // 반응속도 체크
+          endTime.current = new Date();
+          this.setState((prevState) => {
+            return {
+              state: 'waiting',
+              message: '클릭해서 시작하세요.',
+              result: [...prevState.result, this.endTime, this.startTime],
+            };
+          });
         }
-    }
-    onChangeInput = (e) =>{
-        this.setState({value: e.target.value});
-    }
+      };
 
-    onRefInput = (c) =>{
-        this.input= c;
+    renderAverage = () => {
+        return this.state.result.length === 0  // result.length가 0이면 null값을 주고 아니면 아래 코드를 실행 
+        ? null
+        : <div>평균 시간: {this.state.result.reduce((a,c)=>a+c) / this.state.result.length }ms  </div> 
     }
-
     render(){
-        return (
+        return(
             <>
-                <div>{this.state.word}</div>
-                <form onSubmit={this.onSubmitForm}>
-                    <input ref ={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
-                    <button>입력!</button>
-                    <div>{this.state.result}</div>
-                </form>
+            <div
+            id="scrren"
+            className={this.state.state}
+            onClick={this.onClickScreen}>
+                {this.state.message}
+            </div>
+            {this.renderAverage()} 
             </>
-        )
-   }
+        );
+    }
+    
 }
 
 module.exports = ResponseCheck;
